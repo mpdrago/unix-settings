01bc8d8cc89623f57366749aa8300177dc27eef9
Michael Drago <michael.drago@edgehogllc.com>
2023-08-14 09:23:16 -0500

    Update to new alacritty config format

--- setup_alacritty.sh
+++ setup_alacritty.sh
@@ -2,12 +2,15 @@
 echo "Setting up alacritty"
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BINARY="alacritty"
+HOSTNAME="`hostname -s`"
 if [ $(type -P "$BINARY") ]; then
   if [ -f ~/.config/alacritty.yml ]; then
-    echo "Alacritty config already found ~/.config/alacritty.yml"
+    echo "Alacritty config already found ~/.config/alacritty.toml"
   else
-    echo "Installing alacritty config to ~/.config/alacritty.yml"
-    ln -s $SCRIPT_DIR/.config/alacritty.yml ~/.config/alacritty.yml
+    echo "Installing alacritty config to ~/.config/alacritty.toml"
+    ln -s $SCRIPT_DIR/.config/alacritty.toml ~/.config/alacritty.toml
+    ln -s $SCRIPT_DIR/.config/alacritty.local-$HOSTNAME.toml ~/.config/alacritty.local-$HOSTNAME.toml
+    ln -s $SCRIPT_DIR/.config/alacritty.local-$HOSTNAME.toml ~/.config/alacritty.local.toml
   fi
 else
   echo "Alacritty not installed. Executable: $BINARY"
